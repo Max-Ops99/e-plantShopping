8abceef7ec20f23b0237c35a6fcd3e185f9c745b
Jason Bourne <jason@bourne.com>
2026-05-14 16:12:48 -0400

    imported dispatcher and proper import og additem from slices using {}

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css';
-import addItem from './CartSlice';
+import {addItem} from './CartSlice';
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const dispatch = useDispatch();
+
 
     const plantsArray = [
         {
