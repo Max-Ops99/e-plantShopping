db275775159150750aa1fb806eff3911a613fbea
Jason Bourne <jason@bourne.com>
2026-05-14 10:06:29 -0400

    Showing all products card

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import './ProductList.css'
+import './ProductList.css';
+import addItem from './CartSlice';
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
@@ -252,6 +253,17 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true,
+        }));
+    };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -274,7 +286,31 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                {plantsArray.map((category, index) => {
+                    <div key={index}>
+                        <h1>
+                            <div>{category.category}</div>
+                        </h1>
+                        <div className='product-list'>
+                            {category.plants.map((plant, plantIndex) => {
+                                <div className='product-card' key={plantIndex}>
+                                    <img
+                                        className='product-image'
+                                        src={plant.image}
+                                        alt={plant.name}
+                                    />
+                                    <div className='product-title'>{plant.name}</div>
+                                    <div className='product-description'>{plant.description}</div>
+                                    <div className='product-cost'>${plant.cost}</div>
+                                    <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
 
+                                    
+                                </div>
+                            })}
+
+                        </div>
+                    </div>
+                })}
 
                 </div>
             ) : (
