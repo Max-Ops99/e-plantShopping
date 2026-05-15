f67a70f7402764aeefc51edd789d5121665596d7
Jaosn Bourne <jason@bourne.com>
2026-05-15 11:18:58 -0400

    some more bug fix in productlist.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import './ProductList.css';
+import { useDispatch, useSelector } from 'react-redux';
 import {addItem} from './CartSlice';
-import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import './ProductList.css';
 
-function ProductList({ onHomeClick }) {
-    const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+import CartItem from './CartItem';
+import AboutUs from './AboutUs';
+
+function ProductList() {
     const dispatch = useDispatch();
+    const [showCart, setShowCart] = useState(false);
+    const [addedToCart, setAddedToCart] = useState({});
+    const cartItems = useSelector((state) => state.cart.items);
+    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
 
 
     const plantsArray = [
@@ -240,12 +244,16 @@ function ProductList({ onHomeClick }) {
 
     const handleHomeClick = (e) => {
         e.preventDefault();
-        onHomeClick();
+
+        setShowAboutUs(false);
+        setShowCart(false);
+
     };
 
     const handleCartClick = (e) => {
         e.preventDefault();
-        setShowCart(true); // Set showCart to true when cart icon is clicked
+        setShowCart(true); 
+        setShowAboutUs(false);
     };
     const handlePlantsClick = (e) => {
         e.preventDefault();
@@ -258,7 +266,18 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
 
-    const [addedToCart, setAddedToCart] = useState({});
+    
+ 
+
+    const calculateTotalQuantity = () => {
+        return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0):0;
+    };
+
+    const [totalQuantity, setTotalQuantity] = useState(calculateTotalQuantity());
+    useEffect(() => {
+        setTotalQuantity(calculateTotalQuantity());
+    }, [cartItems]);
+
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
 
@@ -285,15 +304,15 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
                 {plantsArray.map((category, index) => (
                     <div key={index}>
-                        <h1>
-                            <div>{category.category}</div>
+                        <h1 className='plantname_heading'>
+                            <div className='plant_heading'>{category.category}</div>
                         </h1>
                         <div className='product-list'>
                             {category.plants.map((plant, plantIndex) => (
@@ -305,7 +324,7 @@ function ProductList({ onHomeClick }) {
                                     />
                                     <div className='product-title'>{plant.name}</div>
                                     <div className='product-description'>{plant.description}</div>
-                                    <div className='product-cost'>${plant.cost}</div>
+                                    <div className='product-cost'>{plant.cost}</div>
                                     <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
 
                                     
