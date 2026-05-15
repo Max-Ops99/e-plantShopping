a41b04677d9d918f3bfe6a187771080a58c8b669
Jaosn Bourne <jason@bourne.com>
2026-05-15 11:29:08 -0400

    Fix bug in cartitem.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItem } from './CreateSlice';
+import { addItem } from './CartSlice';
 import './ProductList.css'; // Import CSS file
 
 import './ProductList.css'; // Import CSS file
